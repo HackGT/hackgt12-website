c9e15c46c4ca8e3baa639a0cd3f6717a16b5337d
crystaltine <donaldcenaaa@outlook.com>
2025-05-19 17:09:28 -0700

    i2

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Heading, HStack } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HStack>
+        <Heading size="4xl">hackgt 12 site initial commit yay!!!</Heading>
+        <Heading size="2xl">whats up git history sleuths</Heading>
+        <Heading size="lg">hi</Heading>
+      </HStack>
     </>
   )
 }
