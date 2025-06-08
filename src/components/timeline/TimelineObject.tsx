54a02955bb1be221bf9ba0d273bd5f1180432d9d
crystaltine <donaldcenaaa@outlook.com>
2025-06-08 00:55:41 -0700

    merge with local stashed

---
+++ src/components/timeline/TimelineObject.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './tlstyles.css';
+
+export type TimelineObject_t = {
+	name: string;
+	desc: string;
+	time: string;
+	icon_path: string;
+}
+
+type ExtraProps = {
+	number: number;
+	is_on_top?: boolean;
+	style?: React.CSSProperties;
+}
+
+const TimelineObject = (props: TimelineObject_t & ExtraProps) => {
+	return (
+		<div 
+		id={`tl-object-${props.number}`}
+		className={`tl-object-wrapper ${props.is_on_top? 'is_top' : 'is_bottom'}`} 
+		style={props.style}>
+			<div className='tl-object-number font-poppins'>{props.number}</div>
+			<div className='tl-object-label'>{props.name}: {props.time}</div>
+			<img className='tl-object-icon' src={props.icon_path} />
+		</div>
+	);
+};
+
+export default TimelineObject;
---
+++ src/components/timeline/TimelinePage.tsx
@@ -0,0 +1,127 @@
+import React, {useCallback, useEffect, useReducer, useRef, useState } from 'react'
+import './tlstyles.css'
+import EventCard from './EventCard';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
+import EVENTS from './events.json';
+import TimelineObject, { type TimelineObject_t } from './TimelineObject';
+
+/** "reverse merges" an array of events, placing consecutive items
+ * in alternating return arrays: 1st ->arr1, 2nd ele -> arr2, 3rd ele -> arr1, ...
+ * 
+ * example:
+ * ["one", "two", "three", "four", "five", "six", "seven"]
+ * gets turned into
+ * ["one", "three", "five", "seven"], ["two", "four", "six"]
+ * 
+ * (this is just for ordering the timeline stuff)
+ */
+function partitionInto2Arrays<T>(parentArray: T[]): [T[], T[]] {
+	const arr1 = [];
+	const arr2 = [];
+	for (let i = 0; i < parentArray.length; i++) {
+		if (i % 2 === 0) arr1.push(parentArray[i]);
+		else arr2.push(parentArray[i]);
+	}
+	return [arr1, arr2];
+}
+
+const PageContainer = ({children}: {children?: React.ReactNode}) =>
+	<div className='font-cormo w-screen snap-start h-screen tlpage-bg flex items-center justify-center'>
+		{children}
+	</div>
+
+const TimelinePage = () => {
+	const timelineLineRef = useRef<HTMLDivElement>(null);
+	const topEventContainerRef = useRef<HTMLDivElement>(null);
+	const bottomEventContainerRef = useRef<HTMLDivElement>(null);
+
+	const [selectedDay, setSelectedDay] = useState<1|2|3>(1);
+	const [selectedEventIdx, setSelectedEventIdx] = useState<number>(0);
+
+	const [timelineHalves, setTimelineHalves] = useState(partitionInto2Arrays<TimelineObject_t>(EVENTS[selectedDay-1] as TimelineObject_t[]));
+
+	const updateIdx = useCallback((newIdx: number) => {
+		const numEventsThisDay = EVENTS[selectedDay-1].length;
+		console.log(`Updating selectedEventIdx to ${newIdx}, # events today: ${numEventsThisDay} (selectedDay: ${selectedDay})`);
+		if (newIdx < 0 || newIdx >= numEventsThisDay) return;
+		else setSelectedEventIdx(newIdx);
+	}, [selectedDay]);
+
+	useEffect(() => {
+		const newHalves = partitionInto2Arrays<TimelineObject_t>(EVENTS[selectedDay-1] as TimelineObject_t[]);
+		setTimelineHalves(newHalves);
+		setSelectedEventIdx(0); // reset to first event of the day
+	}, [selectedDay]);
+
+	// flash effect when switching selectedEventIdx
+	useEffect(() => {
+		const ele = document.getElementById(`tl-object-${selectedEventIdx+1}`);
+		ele?.classList.add('flash');
+		const t = setTimeout(() => {
+			ele?.classList.remove('flash');
+		}, 1500); // important: check the flash animations in ./tlstyles.css, should match this timing 
+
+		return () => {
+			// insta-end animation if they change to another event
+			ele?.classList.remove('flash');
+			clearTimeout(t);
+		}
+	}, [selectedEventIdx]);
+
+	return (
+		<PageContainer>
+			<div className='flex flex-col items-center'>
+				<div className='flex gap-8 items-center'>
+					<button className='eventcard-rounded-button floatleft-on-hover' onClick={() => updateIdx(selectedEventIdx-1)}>
+						<ArrowLeft size="2.5rem" />
+					</button>
+					<EventCard day={selectedDay} idx={selectedEventIdx} />
+					<button className='eventcard-rounded-button floatright-on-hover' onClick={() => updateIdx(selectedEventIdx+1)}>
+						<ArrowRight size="2.5rem" />
+					</button>
+				</div>
+
+				<div className='timeline-container'>
+					<div className='timeline-group top' ref={topEventContainerRef}>
+						{timelineHalves[0].map((ele, index) => (
+							<TimelineObject
+							key={`toptl-${index}`}
+							name={ele.name}
+							time={ele.time}
+							desc={ele.desc}
+							icon_path={ele.icon_path}
+							number={2*index+1}
+							is_on_top />
+						))}
+					</div>
+					<div 
+					className='timeline-line' 
+					style={{width: (Math.max(topEventContainerRef.current?.clientWidth || 0, bottomEventContainerRef.current?.clientWidth || 0) + 50) + 'px'}}
+					ref={timelineLineRef} />
+					<div className='timeline-group bottom' ref={bottomEventContainerRef}>
+						{timelineHalves[1].map((ele, index) => (
+							<TimelineObject
+							key={`bottomtl-${index}`}
+							name={ele.name}
+							time={ele.time}
+							desc={ele.desc}
+							icon_path={ele.icon_path}
+							number={2*index+2}
+							is_on_top={false} />
+						))}
+					</div>
+				</div>
+				<div className='day-selector'>{[1, 2, 3].map((n) => 
+					<div 
+					key={n}
+					className={`day-selector-option ${selectedDay === n? 'selected' : ''}`} 
+					onClick={() => {setSelectedDay(n as 1|2|3); setSelectedEventIdx(0);}}>
+						Day {n}
+					</div>
+				)}</div>
+			</div>
+		</PageContainer>
+	);
+};
+
+export default TimelinePage;
