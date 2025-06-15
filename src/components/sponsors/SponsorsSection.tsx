7915eb0de11eebd9a788084a76e38c5d2771f353
ganning127 <ganningxu@gmail.com>
2025-06-15 12:29:06 -0700

    mobile sections according to figma for about and faq

--- src/components/sponsors/SponsorsSection.tsx
+++ src/components/sponsors/SponsorsSection.tsx
@@ -1,12 +1,14 @@
 export const SponsorsSection = () => {
   return (
     <div>
-      <h1 className="font-cormo text-[96px] color-gold text-center">
+      <h1 className="font-cormo text-[48px] lg:text-[96px] color-gold text-center">
         Sponsors
       </h1>
 
       <div className="min-h-[300px] flex items-center justify-center">
-        <p className="font-poppins text-[30px] text-white">coming soon!!!</p>
+        <p className="font-poppins text-[20px] lg:text-[30px] text-white">
+          coming soon!!!
+        </p>
       </div>
     </div>
   );
