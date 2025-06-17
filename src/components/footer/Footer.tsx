b418bbe5c5ed4c9957a263b008d42b75b45b4428
ganning127 <ganningxu@gmail.com>
2025-06-16 21:24:20 -0700

    lowercase 'hexlabs' in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <div>
       <p className="text-[20px] lg:text-[30px] text-white font-poppins text-center">
-        With &lt;3 from the HexLabs team <br />
+        With &lt;3 from the hexlabs team <br />
         <a
           className="underline"
           href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
