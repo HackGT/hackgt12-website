20d5363f8721ab85c736f3a3955cc1930af6299b
crystaltine <donaldcenaaa@outlook.com>
2025-07-11 00:37:45 -0700

    remove redundant tailwind class in FloorShownOnSmall.tsx

--- src/components/FloorShownOnSmall.tsx
+++ src/components/FloorShownOnSmall.tsx
@@ -3,7 +3,7 @@ export const FloorShownOnSmall = () => {
     <img
       src="/about/about_floor.webp"
       alt="________"
-      className="block lg:hidden horiz-scroll-floor block"
+      className="block lg:hidden horiz-scroll-floor"
     />
   );
 };
