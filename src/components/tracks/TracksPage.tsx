3652265b154f2504158e7f6cd311d8844d67effd
Lauren Ji <lyangji1011@gmail.com>
2025-07-30 21:28:18 -0400

    disappears for normal screen

--- src/components/tracks/TracksPage.tsx
+++ src/components/tracks/TracksPage.tsx
@@ -6,22 +6,31 @@ import {
   DialogTitle,
   DialogDescription,
 } from "@/components/ui/dialog";
+import { useState } from "react";
 
 export const TracksPage = () => {
+  const [isHallOpen, setIsHallOpen] = useState(false);
+  const [isGadgetOpen, setIsGadgetOpen] = useState(false);
+  const [isEmergingOpen, setIsEmergingOpen] = useState(false);
+  const [isCuratorOpen, setIsCuratorOpen] = useState(false);
+  const [isCryptOpen, setIsCryptOpen] = useState(false);
+
   return (
     <div className="snap-start w-screen h-screen" id="room2">
       <div className="relative w-full h-full bg-gradient-to-b from-[#281004] to-[#5b1e19] overflow-hidden">
 
         <div className="absolute inset-0 translate-y-[10vh]">
           {/* Hall of Illusions */}
-          <Dialog>
-            <DialogTrigger asChild>
-              <img
-                src="/tracks/hall_of_illusions.svg"
-                alt="Hall of Illusions"
-                className="absolute top-[10%] lg:right-[73%] xl:right-[69%] w-[32vh] max-w-[288px] min-w-[120px] translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:rotate-2"
-              />
-            </DialogTrigger>
+          <Dialog open={isHallOpen} onOpenChange={setIsHallOpen}>
+            {!isHallOpen && (
+              <DialogTrigger asChild>
+                <img
+                  src="/tracks/hall_of_illusions.svg"
+                  alt="Hall of Illusions"
+                  className="absolute top-[10%] lg:right-[73%] xl:right-[69%] w-[32vh] max-w-[288px] min-w-[120px] translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:rotate-2"
+                />
+              </DialogTrigger>
+            )}
             <DialogContent imageSrc="/tracks/dialog/hall_of_illusions.png" paddingX={18}>
               <DialogHeader>
                 <DialogTitle>
@@ -37,14 +46,16 @@ export const TracksPage = () => {
           </Dialog>
 
           {/* The Gadget Gallery */}
-          <Dialog>
-            <DialogTrigger asChild>
-              <img
-                src="/tracks/gadget_gallery.svg"
-                alt="The Gadget Gallery"
-                className="absolute top-[5%] left-[50%] w-[23vh] max-w-[192px] min-w-[100px] -translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:-rotate-1"
-              />
-            </DialogTrigger>
+          <Dialog open={isGadgetOpen} onOpenChange={setIsGadgetOpen}>
+            {!isGadgetOpen && (
+              <DialogTrigger asChild>
+                <img
+                  src="/tracks/gadget_gallery.svg"
+                  alt="The Gadget Gallery"
+                  className="absolute top-[5%] left-[50%] w-[23vh] max-w-[192px] min-w-[100px] -translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:-rotate-1"
+                />
+              </DialogTrigger>
+            )}
             <DialogContent imageSrc="/tracks/dialog/gadget_gallery.png" isVertical={true} paddingX={22}>
               <DialogHeader>
                 <DialogTitle>
@@ -59,14 +70,16 @@ export const TracksPage = () => {
           </Dialog>
 
           {/* Emerging */}
-          <Dialog>
-            <DialogTrigger asChild>
-              <img
-                src="/tracks/emerging.svg"
-                alt="Emerging"
-                className="absolute top-[15%] lg:left-[73%] xl:left-[69%] w-[32vh] max-w-[281px] min-w-[120px] -translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:rotate-1"
-              />
-            </DialogTrigger>
+          <Dialog open={isEmergingOpen} onOpenChange={setIsEmergingOpen}>
+            {!isEmergingOpen && (
+              <DialogTrigger asChild>
+                <img
+                  src="/tracks/emerging.svg"
+                  alt="Emerging"
+                  className="absolute top-[15%] lg:left-[73%] xl:left-[69%] w-[32vh] max-w-[281px] min-w-[120px] -translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:rotate-1"
+                />
+              </DialogTrigger>
+            )}
             <DialogContent imageSrc="/tracks/dialog/emerging.png" paddingX={18}>
               <DialogHeader>
                 <DialogTitle>
@@ -82,14 +95,16 @@ export const TracksPage = () => {
           </Dialog>
 
           {/* The Curator's Cause */}
-          <Dialog>
-            <DialogTrigger asChild>
-              <img
-                src="/tracks/curators_cause.svg"
-                alt="The Curator’s Cause"
-                className="absolute top-[38%] lg:right-[64%] xl:right-[61%] w-[35vh] max-w-[326px] min-w-[150px] translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:rotate-1"
-              />
-            </DialogTrigger>
+          <Dialog open={isCuratorOpen} onOpenChange={setIsCuratorOpen}>
+            {!isCuratorOpen && (
+              <DialogTrigger asChild>
+                <img
+                  src="/tracks/curators_cause.svg"
+                  alt="The Curator’s Cause"
+                  className="absolute top-[38%] lg:right-[64%] xl:right-[61%] w-[35vh] max-w-[326px] min-w-[150px] translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:rotate-1"
+                />
+              </DialogTrigger>
+            )}
             <DialogContent imageSrc="/tracks/dialog/curators_cause.png" paddingX={24}>
               <DialogHeader>
                 <DialogTitle>
@@ -104,14 +119,16 @@ export const TracksPage = () => {
           </Dialog>
 
           {/* Crypt of Data */}
-          <Dialog>
-            <DialogTrigger asChild>
-              <img
-                src="/tracks/crypt_of_data.svg"
-                alt="Crypt of Data"
-                className="absolute top-[47%] lg:right-[35%] xl:right-[38%] w-[33vh] max-w-[306px] min-w-[140px] translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:-rotate-2"
-              />
-            </DialogTrigger>
+          <Dialog open={isCryptOpen} onOpenChange={setIsCryptOpen}>
+            {!isCryptOpen && (
+              <DialogTrigger asChild>
+                <img
+                  src="/tracks/crypt_of_data.svg"
+                  alt="Crypt of Data"
+                  className="absolute top-[47%] lg:right-[35%] xl:right-[38%] w-[33vh] max-w-[306px] min-w-[140px] translate-x-1/2 cursor-pointer transition-transform duration-300 hover:scale-108 hover:-rotate-2"
+                />
+              </DialogTrigger>
+            )}
             <DialogContent imageSrc="/tracks/dialog/crypt_of_data.png" paddingX={19}>
               <DialogHeader>
                 <DialogTitle>
